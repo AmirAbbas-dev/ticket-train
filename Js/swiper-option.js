0a60ea7882bc41f23b1a6f5b7a799f50b2e09cbf
Amir Abbas Hassan Khani <89020912+AmirAbbas-dev@users.noreply.github.com>
2022-07-12 21:22:06 +0430

    Update swiper-option.js

--- Js/swiper-option.js
+++ Js/swiper-option.js
@@ -1,6 +1,6 @@
 var swiper = new Swiper(".swiper-ticket", 
     {
-        slidesPerView: 3.5,
+        slidesPerView: 'auto',
         spaceBetween: 30,
         grabCursor: !0,
         speed: 2e3,
